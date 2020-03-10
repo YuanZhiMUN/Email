d017a6487a4ac016c270a660f8a86c740f515693
Yuan Zhi <zy8715@mun.ca>
2020-03-10 16:37:32 -0230

    Add validators and error dealers

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,14 +4,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { HttpClient } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
